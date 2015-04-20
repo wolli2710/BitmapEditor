c2d3077965a12fbd121cda0b982816efff268ff9
Wolfgang Vogl <wolfgang.vogl01@gmail.com>
2015-04-20 23:03:56 +0100

    initial commit created InputManager

---
+++ lib/input_manager.rb
@@ -0,0 +1,28 @@
+class InputManager
+  @@running = true
+  @user_input 
+
+  def initialize
+    InputManager.run
+  end
+
+  protected
+
+  def self.run
+    while @@running do
+      @user_input = STDIN.gets.chomp
+      InputManager.handle_user_input @user_input
+    end
+    puts "Session ended"
+  end
+
+  def self.handle_user_input(user_input)
+    instructions_array = user_input.split(' ')
+    if(instructions_array[0] == 'X')
+      @@running = false
+    end
+  end
+
+end
+
+InputManager.new
